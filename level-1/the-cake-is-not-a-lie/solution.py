7e54b1d372d1da79c2cdc3999501fbbab6bf5696
moonpie <eoinm@esatclear.ie>
2021-04-30 09:00:39 +0000

    Submitted: Level 1 - The cake is not a lie!

--- level-1/the-cake-is-not-a-lie/solution.py
+++ level-1/the-cake-is-not-a-lie/solution.py
@@ -1,2 +1,18 @@
 def solution(s):
-    
+
+    max_slices = 0
+    min_leftovers = len(s)
+
+    for slice_size in range(len(s)):
+        if slice_size == 0:
+            continue
+        for start_index in range(len(s) - slice_size):
+            pattern = s[start_index:start_index + slice_size]
+            slices = s.count(pattern)
+            leftovers = len(s) - (len(pattern) * slices)
+            if leftovers < min_leftovers:
+                min_leftovers = leftovers
+                max_slices = slices
+            start_index += 1
+
+    return max_slices
